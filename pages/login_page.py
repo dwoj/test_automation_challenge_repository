b5fe2c780785258cc407fdc51d0bce3e6c9ea4d8
dwoj <dom.wojtecka@gmail.com>
2023-04-17 18:53:54 +0200

    task2_refactor

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,7 +4,7 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     login_field_xpath = "//*[@id='login']"
     password_field_xpath = "//*[@id='password']"
-    sign_in_button_xpath = "//*[@id='__next']/form/div/div[2]/button/span[1]"
+    sign_in_button_xpath = "//*[contains(@class, 'MuiButton-label')]"
 
     def type_in_email(self, email):
         self.field_send_keys(self.login_field_xpath, email)
